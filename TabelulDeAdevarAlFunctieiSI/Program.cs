60ce6010c35ce4ef6439b5279970d3fd7edcc5a6
briansomogyi <brian.somogyi@emanuel.ro>
2023-10-12 16:17:41 +0300

    Done some changes

---
+++ TabelulDeAdevarAlFunctieiSI/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TabelulDeAdevarAlFunctieiSI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition1 = true, condition2 = true;
+            Console.WriteLine("Bitwise AND Truth Table");
+            Console.WriteLine($"True AND True = {condition1 & condition2}");
+            condition2 = false;
+            Console.WriteLine($"True AND False = {condition1 & condition2}");
+            condition1 = false;
+            Console.WriteLine($"False AND False = {condition1 & condition2}");
+            condition2 = true;
+            Console.WriteLine($"False AND True = {condition1 & condition2}");
+        }
+    }
+}
